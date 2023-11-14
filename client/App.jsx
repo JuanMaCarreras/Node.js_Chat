610300ac5626ce7089f95c3617922b29cbc37c70
JuanMaCarrearas <manuucarreras@gmail.com>
2023-11-13 21:26:29 -0300

    app

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,58 @@
 import io from 'socket.io-client'
+import { useState, useEffect } from 'react'
 
 const socket = io('/')
 
 function App () {
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newMessage = {
+      body: message,
+      from: 'Me'
+    }
+    setMessages([...messages, newMessage])
+    socket.emit('message', message)
+  }
+
+  useEffect(() => {
+    socket.on('message', reciveMessages)
+
+    return () => {
+      socket.off('message', reciveMessages)
+    }
+  }, [])
+
+  const reciveMessages = message => {
+    setMessages(state => [...state, message])
+  }
+
   return (
-    <div>Hola Mundo!!!</div>
+    <div>
+      <ul className=''>
+        {
+          messages.map((message, index) => (
+            <li className='text-3xl' key={index}>{message.from}:{message.body}</li>
+          ))
+        }
+      </ul>
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type='text' placeholder='Write message....'
+          onChange={(e) => setMessage(e.target.value)}
+        />
+
+        <button>
+          Send
+        </button>
+
+      </form>
+
+    </div>
   )
 }
 
